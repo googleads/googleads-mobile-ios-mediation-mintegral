a4b03d72525fb2df5caf5c5b9af0ce7303087a63
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2026-05-04 14:20:18 -0700

    Updating Mintegral iOS adapter for release version 8.1.3.
    PiperOrigin-RevId: 910239453

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/Mintegral-official/MintegralAdSDK-Swift-Package.git",
-      exact: "8.1.1"
+      exact: "8.1.3"
     ),
     .package(
       url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
@@ -48,7 +48,7 @@ let package = Package(
     .binaryTarget(
       name: "MintegralAdapter",
       url:
-        "https://dl.google.com/googleadmobadssdk/mediation/ios/mintegral/MintegralAdapter-8.1.1.0.zip",
+        "https://dl.google.com/googleadmobadssdk/mediation/ios/mintegral/MintegralAdapter-8.1.3.0.zip",
       checksum: "7666f960e9f8afacfacead7e913332a3a47c34877e30321a40df125fa59b636f"
     ),
   ]
